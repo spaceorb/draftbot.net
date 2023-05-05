0d37c0c20b76b5e88b3d9ff5975133d956edf56d
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2023-05-05 01:21:07 -0700

    uibuild

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const logger = require("./logger");
 require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "build")));
-app.use(cors({ origin: "https://www.draftbot.net" }));
+
 // Always return the main index.html for any unmatched route
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"));
@@ -42,7 +42,7 @@ mongoose
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://draftbot.net"],
+    origin: ["http://localhost:3000", "https://www.draftbot.net"],
     methods: ["GET", "POST"],
   },
 });
