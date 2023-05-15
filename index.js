6f4daf3d7fc9f1d5544b7fedb31cfff9e971bbdf
spaceorb <kennethlee@Kenneths-MacBook-Pro.local>
2023-05-14 23:36:43 -0700

    uibuild

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ mongoose
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:3000",
+      "http://localhost:3001",
       "https://draftbot.net",
       "https://www.draftbot.net",
     ],
